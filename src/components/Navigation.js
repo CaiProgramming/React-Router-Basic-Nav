dceead8e5c270e9220af2f9d35e366cabea945bb
Cai Martin <cmartin.professional@yahoo.com>
2019-06-10 16:00:08 -0400

    Finished Nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,31 @@
-import React from 'react';
-
-const Navigation = () => {
-  return (
-    <div>
-      <div className="App">
-        <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
+import React from "react";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+const List = styled.li`
+  font-size: 2rem;
+  text-decoration: none;
+  list-style: none;
+  cursor: pointer;
+`;
+class Navigation extends React.Component {
+  PathHandler = page => {
+    this.props.history.push(page);
+  };
+  render() {
+    return (
+      <div>
+        <div className="App">
+          <h1>React Router Mini</h1>
+          <List onClick={() => this.PathHandler("/")}>Home</List>
+          <List onClick={() => this.PathHandler("/About")}>About</List>
+          <List onClick={() => this.PathHandler("/Contact")}>Contact</List>
+          <List onClick={() => this.PathHandler("/Navigation")}>
+            Navigation
+          </List>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-export default Navigation;
+export default withRouter(Navigation);
